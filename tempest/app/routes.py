1de93f0a8a0c295abb24cfcf50d0ab6977b60dd8
Simon Garton <simon.garton@gmail.com>
2023-04-01 18:55:22 +1300

    UI improvements.

--- tempest/app/routes.py
+++ tempest/app/routes.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 from tempest.app import app
 from tempest import tempest
 from datetime import datetime
@@ -13,6 +13,7 @@ import json
 def index():
     data = tempest.build_root_data()
     return jsonify(data)
+
 
 @app.route('/heartbeat', methods=['POST'])
 def post_heartbeat():
@@ -31,6 +32,36 @@ def get_meters():
         return jsonify(tempest.get_meters())
     return jsonify(tempest.get_meter(serial))
 
+
 @app.route('/update', methods=['POST'])
 def update():
     return jsonify(tempest.update(request.json))
+
+
+@app.route('/ui/heartbeats', methods=['GET'])
+def ui_heartbeats():
+    heartbeat_data = tempest.get_heartbeats()
+    heartbeats = []
+    for k,e in heartbeat_data.items():
+        heartbeats.append(
+            {
+                'serial': k,
+                'timestamp': e
+             }
+        )
+    return render_template('heartbeats.html', heartbeats=heartbeats)
+
+@app.route('/ui/meters', methods=['GET'])
+def ui_meters():
+    meter_data = tempest.get_meters()
+    meters = []
+    for k,e in meter_data.items():
+        meters.append(
+            {
+                'serial': k,
+                'count': e['count'],
+                'latest': e['latest'],
+                'last_seen': e['last_seen'],
+             }
+        )
+    return render_template('meters.html', meters=meters)
