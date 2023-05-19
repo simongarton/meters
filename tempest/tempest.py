188073f87db777b01277cf7f4be21092f1ea8e3d
Simon Garton <simon.garton@gmail.com>
2023-05-19 16:08:44 +1200

    New methods on Tempest to support the API.

--- tempest/tempest.py
+++ tempest/tempest.py
@@ -8,15 +8,15 @@ import requests
 #
 # 0.2.0 : 2023-05-05 secrets as json, two endpoints
 
-with open('secrets.json', 'r') as config_file:
+with open("secrets.json", "r") as config_file:
     secrets = json.load(config_file)
 
-PIPELINE_URL=secrets['url']
-API_KEY=secrets['api_key']
+PIPELINE_URL = secrets["url"]
+API_KEY = secrets["api_key"]
 
-TIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
-DAY_FORMAT = '%Y-%m-%d'
-VERSION='0.2.0'
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"  # not used
+DAY_FORMAT = "%Y-%m-%d"
+VERSION = "0.2.0"
 
 CONVERT_TO_PIPELINE = False
 
@@ -28,10 +28,10 @@ def log(message):
     if not ENABLE_SERVER_LOG:
         print(message)
         return
-    filename = 'tempest_server.log'
-    mode = 'a' if os.path.exists(filename) else 'w'
+    filename = "tempest_server.log"
+    mode = "a" if os.path.exists(filename) else "w"
     with open(filename, mode) as output:
-        output.write('{} : {}\n'.format(datetime.now(), message))
+        output.write("{} : {}\n".format(datetime.now(), message))
 
 
 def get_ip():
@@ -42,40 +42,52 @@ def get_ip():
     return ip
 
 
+def now_to_second():
+    return datetime.now().replace(second=0, microsecond=0)
+
+
 def build_root_data():
     data = {
-        'entity': 'tempest',
-        'ip': get_ip(),
-        'timestamp': datetime.now()
+        "entity": "tempest",
+        "ip": get_ip(),
+        "now": now_to_second().isoformat(),
+        "timestamp": round(datetime.now().timestamp()),
     }
     return data
 
 
 def create_or_get_meters():
-    if not os.path.exists('data'):
-        os.mkdir('data')
-    filename = 'data/meters.json'
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    filename = "data/meters.json"
     if os.path.exists(filename):
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             data = json.load(file)
         return data
     else:
         return {}
 
 
-def save_meters(data):
-    filename = 'data/meters.json'
-    with open(filename, 'w') as file:
+def save_meter_data(data):
+    filename = "data/meters.json"
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
     return data
 
 
+def convert_heartbeats_to_list(data):
+    list = []
+    for serial_number, last_seen in data.items():
+        list.append({"serialNumber": serial_number, "timeLastCommunicated": last_seen})
+    return list
+
+
 def create_or_get_heartbeats():
-    if not os.path.exists('data'):
-        os.mkdir('data')
-    filename = 'data/heartbeats.json'
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    filename = "data/heartbeats.json"
     if os.path.exists(filename):
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             data = json.load(file)
         return data
     else:
@@ -83,79 +95,202 @@ def create_or_get_heartbeats():
 
 
 def save_heartbeats(data):
-    filename = 'data/heartbeats.json'
-    with open(filename, 'w') as file:
+    filename = "data/heartbeats.json"
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
     return data
-    
+
 
 def get_heartbeats():
     data = create_or_get_heartbeats()
-    return data
+    return convert_heartbeats_to_list(data)
 
 
 def post_heartbeat(heartbeat):
-    log('heartbeat {}'.format(heartbeat))
+    log("heartbeat {}".format(heartbeat))
     print(heartbeat)
     data = create_or_get_heartbeats()
-    data[heartbeat['serial']] = datetime.strftime(datetime.now(), TIME_FORMAT)
+    data[heartbeat["serial"]] = now_to_second().isoformat()
     save_heartbeats(data)
     return data
 
 
+def convert_meters_to_list(data):
+    list = []
+    for serial_number, meter_data in data.items():
+        # temporary, for old data
+        time_key = (
+            "timeLastCommunicated"
+            if "timeLastCommunicated" in meter_data
+            else "last_seen"
+        )
+        count_key = (
+            "countOfFilesAvailable"
+            if "countOfFilesAvailable" in meter_data
+            else "count"
+        )
+        latest_key = "latestFile" if "latestFile" in meter_data else "latest"
+        last_communicated = datetime.fromisoformat(meter_data[time_key])
+        elapsed_seconds = round(
+            datetime.now().timestamp() - last_communicated.timestamp()
+        )
+        list.append(
+            {
+                "serialNumber": serial_number,
+                "timeLastCommunicated": meter_data[time_key],
+                "elapsedSeconds": elapsed_seconds,
+                "countOfFilesAvailable": meter_data[count_key],
+                "latestFile": meter_data[latest_key],
+            }
+        )
+    return list
+
+
 def get_meters():
-    return create_or_get_meters()
+    data = create_or_get_meters()
+    return convert_meters_to_list(data)
+
+def get_meter_payload_datastream(serial_number, payload_date, datastream_name):
+    dirname = "data/{}".format(serial_number)
+    if not os.path.exists(dirname):
+        return {}
+    filename = "data/{}/{}.json".format(serial_number, payload_date)
+    with open(filename, "r") as input:
+        payload = json.load(input)
+    datastream_data =  payload["datastreams"][datastream_name]
+    data = []
+    for timestamp, value in datastream_data.items():
+        data.append({
+            'timestamp':timestamp,
+            'value':value
+        })
+    data.sort(key= lambda x:x['timestamp'])
+    return data
+
+
+def get_meter_payload_datastreams(serial_number, payload_date):
+    dirname = "data/{}".format(serial_number)
+    if not os.path.exists(dirname):
+        return {}
+    filename = "data/{}/{}.json".format(serial_number, payload_date)
+    with open(filename, "r") as input:
+        payload = json.load(input)
+    payload_datastreams = []
+    for datastream_name, datastream_data in payload["datastreams"].items():
+        total_value = 0
+        earliest = None
+        latest = None
+        count = 0
+        for timestamp, value in datastream_data.items():
+            count = count + 1
+            total_value = total_value + value
+            reading_time = datetime.fromisoformat(timestamp)
+            if not earliest or (earliest - reading_time).seconds < 0:
+                earliest = reading_time
+            if not latest or (latest - reading_time).seconds > 0:
+                latest = reading_time
+        payload_datastreams.append(
+            {
+                "name": datastream_name,
+                "measurements": count,
+                "totalValue": round(1000 * total_value) / 1000,
+                "earliest": earliest.isoformat(),
+                "latest": latest.isoformat(),
+            }
+        )
+    return payload_datastreams
+
+
+def get_meter_payload(serial_number, payload_date):
+    dirname = "data/{}".format(serial_number)
+    if not os.path.exists(dirname):
+        return {}
+    filename = "data/{}/{}.json".format(serial_number, payload_date)
+    with open(filename, "r") as input:
+        payload = json.load(input)
+    return payload
+
+
+def get_meter_payloads(serial_number):
+    dirname = "data/{}".format(serial_number)
+    if not os.path.exists(dirname):
+        return []
+    meter_files = []
+    for payload_date in os.listdir(dirname):
+        filename = "data/{}/{}".format(serial_number, payload_date)
+        with open(filename, "r") as input:
+            payload = json.load(input)
+        snapshots = len(payload["snapshots"])
+        datastreams = len(payload["datastreams"])
+        interval = payload["interval"]
+        metadata = payload["metadata"]
+        meter_version = metadata["version"] if "version" in metadata else "?"
+        data_model_version = (
+            metadata["data_model_version"] if "data_model_version" in metadata else "?"
+        )
+        meter_files.append(
+            {
+                "filename": filename,
+                "meterVersion": meter_version,
+                "dataModelVersion": data_model_version,
+                "payloadDate": payload_date,
+                "interval": interval,
+                "snapshots": snapshots,
+                "datastreams": datastreams,
+            }
+        )
+    return meter_files
 
 
 def get_meter(serial):
-    dirname = 'data/{}'.format(serial)
+    dirname = "data/{}".format(serial)
     if not os.path.exists(dirname):
         os.mkdir(dirname)
     days = []
     for file in os.listdir(dirname):
-        days.append(file.replace('.json', ''))
+        days.append(file.replace(".json", ""))
     days.sort(reverse=True)
     return days
 
 
 def get_meter_readings(serial, day):
-    dirname = 'data/{}'.format(serial)
+    dirname = "data/{}".format(serial)
     if not os.path.exists(dirname):
         os.mkdir(dirname)
-    filename = 'data/{}/{}.json'.format(serial, day)
+    filename = "data/{}/{}.json".format(serial, day)
     if not os.path.exists(filename):
         return []
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         data = json.load(file)
     readings = []
-    skip_keys = ['serial', 'reading_day']
-    for k,e in data.items():
+    skip_keys = ["serial", "reading_day"]
+    for k, e in data.items():
         if k in skip_keys:
             continue
-        readings.append({
-            'timestamp': k,
-            'value': e
-        })
+        readings.append({"timestamp": k, "value": e})
     return readings
 
 
-
-def save_data(serial, date, data):
-    dirname = 'data/{}'.format(serial)
+def save_all_data(serial, date, data):
+    dirname = "data/{}".format(serial)
     if not os.path.exists(dirname):
         os.mkdir(dirname)
-    filename = 'data/{}/{}.json'.format(serial, date)
-    with open(filename, 'w') as file:
+
+    filename = "data/{}/{}.json".format(serial, date)
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
     file_count = len(os.listdir(dirname))
+
     meter_data = create_or_get_meters()
     entry = {
-        'count':file_count,
-        'latest': date,
-        'last_seen': datetime.strftime(datetime.now(), TIME_FORMAT)
+        "serialNumber": serial,
+        "timeLastCommunicated": now_to_second().isoformat(),
+        "countOfFilesAvailable": file_count,
+        "latestFile": date,
     }
+
     meter_data[serial] = entry
-    save_meters(meter_data)
+    save_meter_data(meter_data)
 
     return data
 
@@ -163,33 +298,29 @@ def save_data(serial, date, data):
 def convert_to_pipeline_format(data):
     # this is the ProcessingPayload format from the pipeline.
     meter = {}
-    meter['serialNumber'] = data['serial']
+    meter["serialNumber"] = data["serial"]
 
     unit_of_work = {}
-    unit_of_work['serialNumber'] = data['serial']
+    unit_of_work["serialNumber"] = data["serial"]
     # really not sure about this - how do I know ?
-    unit_of_work['payloadDate'] = '{}T00:00:00+12:00'.format(data['reading_day'])
+    unit_of_work["payloadDate"] = "{}T00:00:00+12:00".format(data["reading_day"])
 
     datastreams = []
-    for name, reading_data in data['datastreams'].items():
+    for name, reading_data in data["datastreams"].items():
         datastream = {}
-        datastream['name'] = name
-        datastream['interval'] = 5 # TODO this should come from metadata but is missing.
-        
+        datastream["name"] = name
+        datastream[
+            "interval"
+        ] = 5  # TODO this should come from metadata but is missing.
+
         readings = []
         for timestamp, value in reading_data.items():
-            readings.append({
-                'timestamp': timestamp,
-                'value': value
-            })
-        datastream['readings'] = readings
+            readings.append({"timestamp": timestamp, "value": value})
+        datastream["readings"] = readings
         datastreams.append(datastream)
 
-    unit_of_work['dataStreams'] = datastreams
-    converted_data = {
-        'meter': meter,
-        'unitOfWork': unit_of_work
-    }
+    unit_of_work["dataStreams"] = datastreams
+    converted_data = {"meter": meter, "unitOfWork": unit_of_work}
 
     return converted_data
 
@@ -197,35 +328,44 @@ def convert_to_pipeline_format(data):
 def get_headers():
     return {
         # 'x-api-key': tempest_secrets.API_KEY,
-        'x-api-key': API_KEY,
-        'Content-Type': 'application/json' 
+        "x-api-key": API_KEY,
+        "Content-Type": "application/json",
     }
 
+
 def remove_metadata(data):
-    data.pop('metadata')
+    data.pop("metadata")
     return data
-    
+
 
 def upload_to_pipeline(serial, date, data):
-    converted_data = convert_to_pipeline_format(data) if CONVERT_TO_PIPELINE else remove_metadata(data)
+    converted_data = (
+        convert_to_pipeline_format(data)
+        if CONVERT_TO_PIPELINE
+        else remove_metadata(data)
+    )
     # url = tempest_secrets.PIPELINE_URL
-    url = PIPELINE_URL + '/ingestions/processing' if CONVERT_TO_PIPELINE else PIPELINE_URL + '/ingestions/picos'
+    url = (
+        PIPELINE_URL + "/ingestions/processing"
+        if CONVERT_TO_PIPELINE
+        else PIPELINE_URL + "/ingestions/picos"
+    )
     headers = get_headers()
-    log('uploading {}/{} to pipeline @ {} : {}'.format(serial, date, url, headers))
+    log("uploading {}/{} to pipeline @ {} : {}".format(serial, date, url, headers))
     response = requests.post(url, json=converted_data, headers=headers)
     log(response)
-    
+
 
 def update(data):
-    serial = data['serial']
-    date = data['reading_day']
-    log('updating {}@{}'.format(serial, date))
-    save_data(serial, date, data)
+    serial = data["serial"]
+    date = data["reading_day"]
+    log("updating {}@{}".format(serial, date))
+    save_all_data(serial, date, data)
     upload_to_pipeline(serial, date, data)
 
 
 # startup
 if __name__ == "__main__":
-    filename = 'tempest_server.log'
+    filename = "tempest_server.log"
     if os.path.exists(filename):
         os.remove(filename)
---
+++ tempest/test_tempest.py
@@ -0,0 +1,23 @@
+
+import tempest
+
+print(tempest.build_root_data())
+print()
+
+print(tempest.get_heartbeats())
+print()
+
+print(tempest.get_meters())
+print()
+
+print(tempest.get_meter_payloads('pico-001'))
+print()
+
+print(tempest.get_meter_payload('pico-001', '2023-05-16'))
+print()
+
+print(tempest.get_meter_payload_datastreams('pico-001', '2023-05-16'))
+print()
+
+print(tempest.get_meter_payload_datastream('pico-001', '2023-05-16', 'House'))
+print()
